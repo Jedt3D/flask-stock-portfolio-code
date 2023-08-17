9e11da86bae54a669d0c4d245345a8b1a64bd8c9
Worajedt Sitthidumrong <sjedt@3ddaily.com>
2023-08-18 04:47:49 +0700

    merge with macOS version. more templates and comments.

--- app.py
+++ app.py
@@ -1,32 +1,55 @@
-from flask import Flask, escape
+from flask import Flask, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# Static routing
 
 
-@app.route('/hello/<message>')
-def hello_message(message):
-    return f'<h1>Welcome {escape(message)}!</h1>'
+@app.route("/")
+def index():
+    # return "Hello World!"
+    return render_template("index.html")
 
 
-@app.route('/blog_post/<int:post_id>')
-def blog_post(post_id):
-    return f'<h1>Blog Post #{post_id}...</h1>'
-
-
-@app.route('/about', methods=['GET'])
+# @app.route("/about/") default method is GET.
+# Also we can use more than one, like this ["GET", "POST"]
+@app.route("/about/", methods=["GET"])
 def about():
-    return '<h2>About this application ... </h2>'
+    # return "<h2>About this Flask application</h2>"
+    return render_template("about.html", app="Flask Application", version="2.3.2")
 
 
-@app.route('/stocks/', methods=['GET', 'POST'])
-def stocks():
-    return '<h2>Stock List ... </h2>'
+"""
+Note about "/" (trailing slash) in URL.
+Think like a file system.
+✅ when it looks like a folder. "/users/"
+❌ when it looks like a file. "/users/1.txt"
+"""
+
+# Variable routing
+"""
+Using type hinting, also the str built-in type is able
+to use some string validation function and string transformation
+
+Actually it's called Variable Route Types
+string(default), int, path, uuid
+"""
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/hi/<string:name>")
+def hi(name):
+    """
+    A function that takes in a name as a parameter and returns a greeting message.
+
+    Parameters:
+    - name (str): The name to be greeted.
+
+    Returns:
+    - str: A greeting message based on the provided name.
+    """
+    if name.isalpha():
+        name = f"{escape(name.capitalize())}"
+        return render_template("hi.html", name=name)
+    else:
+        return render_template("hi.html", name="")
